015ecb9fcf040ceeedbce344ddceed4e824d7e4f
suwattikul <suwattikulkhet@hotmail.com>
2024-08-20 16:51:55 +0700

    first commit

---
+++ app copy.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="w-screen h-screen flex justify-center items-center bg-rose-300">
+    <div class="bg-green-500 w-[800px] h-[800px] flex justify-center items-center">
+      <div class="bg-white  w-[500px] h-[500px]">
+        <div class="grid grid-cols-4 ">
+  <div class="bg-red-500 col-span-2">01</div>
+  <div class="bg-blue-500 col-span-2">05</div>
+</div>
+<div class="grid grid-cols-4 ">
+  <div class="col-span-2">03</div>
+  <div class="bg-yellow-500 col-span-2">04</div>
+</div>
+<div class="bg-red-500 col-span-2 text-center">05</div>
+<div class="grid grid-cols-4 ">
+  <div class=" col-span-4"></div>
+  <div class=" col-span-2"></div>
+  <div class="bg-blue-500 col-span-2 text-center">06</div>
+</div>
+<div class="grid grid-cols-4 ">
+  <div class="bg-red-500 col-span-2 text-center">07</div>
+  <div class="bg-whlit"></div>
+</div>
+<div class="bg-blue-500 col-span-2 text-center">08</div>
+<div class="bg-ping-500 col-span-2 text-center">09</div>
+<div class="bg-yellow-200 col-span-2 text-center">10</div>
+<div class="grid grid-cols-6 grid-rows-4 ">
+<div class="bg-blue-500">10.1</div>
+  <div class="bg-yellow-500">10.2</div>
+  <div class="bg-red-500">10.3</div>
+  <div class="bg-rose-700">10.4</div>
+  <div class="bg-yellow-700">10.5</div>
+  <div class="bg-red-700">10.5</div>
+</div>
+</div>
+    </div>
+    </div>
+   
+
+</template>
+
---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <div class="border border-purple-600w-full h-screen flex justify-center items-center gap-6">
+    <div class="border-8 border-gray-400 grid grid-cols-1 h-[500px] w-[500px]">
+  <div class="bg-red-500 col-span-2 flex justify-center items-center ">Box1</div>
+  <div class="bg-blue-500 col-span-2 flex justify-center items-center ">Box2</div>
+  </div>
+        <div class="border-8 border-gray-400 grid grid-cols-4 h-[500px] w-[500px]">
+  <div class="bg-red-500 col-span-2 flex justify-center items-center ">Box1</div>
+  <div class="bg-blue-500 col-span-2 flex justify-center items-center ">Box2</div>
+  
+        </div>
+        <div class="border-8 border-gray-400 grid grid-cols-6 h-[500px] w-[500px]">
+  <div class="bg-red-500 col-span-2 flex justify-center items-center ">Box1</div>
+  <div class="bg-blue-500 col-span-2 flex justify-center items-center ">Box2</div>
+  <div class="bg-rose-700 col-span-2 flex justify-center items-center ">Box2</div>
+  </div>
+  </div>  
+</div>
+
+</template>
+
