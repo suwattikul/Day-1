23521907a5b19554f1a249e16e83e44f17670821
suwattikul <suwattikulkhet@hotmail.com>
2024-08-20 17:05:38 +0700

    uodate==> app.will

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  Khet
   <div class="border border-purple-600w-full h-screen flex justify-center items-center gap-6">
     <div class="border-8 border-gray-400 grid grid-cols-1 h-[500px] w-[500px]">
   <div class="bg-red-500 col-span-2 flex justify-center items-center ">Box1</div>
